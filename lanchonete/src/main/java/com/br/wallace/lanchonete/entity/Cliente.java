b7d014c9262fec42c2be8b2813ab6bba0dffb09c
Amaral <dmrramaral@gmail.com>
2023-04-25 20:12:23 -0300

    Alteração de local e pasta do projeto

--- lanchonete/src/main/java/com/br/wallace/lanchonete/entity/Cliente.java
+++ lanchonete/src/main/java/com/br/wallace/lanchonete/entity/Cliente.java
@@ -1,4 +1,4 @@
-package entity;
+package com.br.wallace.lanchonete.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -7,32 +7,33 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Data
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "tb_cliente")
 public class Cliente {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(name = "nome")
-	private String nome;
+    @Column(name = "nome_cliente")
+    private String nome;
 
-	@Column(name = "email")
-	private String email;
+    @Column(name = "cpf_cliente")
+    private String telefone;
 
-	@Column(name = "telefone")
-	private String telefone;
-
-	@Column(name = "cpf")
-	    private String cpf;
+    @Column(name = "email_cliente")
+    private String email;
+    @Column(name = "senha_cliente")
+    private String password;
 
 }
