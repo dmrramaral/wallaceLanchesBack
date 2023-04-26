f96db977dc541b877ae1b37118ff2a9d665efabe
Aluno Tarde <you@example.com>
2023-04-26 16:00:21 -0300

    Ajuste para popular o BD

---
+++ lanchonete/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_cliente (id, nome_cliente, telefone_cliente, cpf_cliente, email_cliente, senha_cliente) 
+VALUES 
+(1, 'Fulano de Tal', '(11) 99999-9999', '123.456.789-10', 'fulano@teste.com', 'senha123'),
+(2, 'Ciclano de Tal', '(11) 88888-8888', '987.654.321-00', 'ciclano@teste.com', 'senha456'),
+(3, 'Beltrano de Tal', '(11) 77777-7777', '111.222.333-44', 'beltrano@teste.com', 'senha789');
