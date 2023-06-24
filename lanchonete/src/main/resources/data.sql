56299616fb37aaf2909d1d3f5adaee5be426c520
Amaral <dmrramaral@gmail.com>
2023-06-24 11:41:24 -0300

    Inserindo itens

--- lanchonete/src/main/resources/data.sql
+++ lanchonete/src/main/resources/data.sql
@@ -28,10 +28,26 @@ INSERT INTO INGREDIENTES (ID, TIPO_INGREDIENTE_ID, NOME_INGREDIENTE) VALUES (12,
 INSERT INTO INGREDIENTES (ID, TIPO_INGREDIENTE_ID, NOME_INGREDIENTE) VALUES (13, 4, 'Pão de Hamburguer');
 
 -- Inserção dos sanduíches
-INSERT INTO tb_sanduiche (id, nome_sanduiche, valor_sanduiche) VALUES (1, 'X-Tudo', 20.00);
+INSERT INTO tb_sanduiche (id, nome_sanduiche, valor_sanduiche) VALUES (1, 'X-Tudo', 21.00);
 INSERT INTO tb_sanduiche (id, nome_sanduiche, valor_sanduiche) VALUES (2, 'X-Burguer', 10.00);
 INSERT INTO tb_sanduiche (id, nome_sanduiche, valor_sanduiche) VALUES (3, 'X-Salada', 15.00);
-INSERT INTO tb_sanduiche (id, nome_sanduiche, valor_sanduiche) VALUES (4, 'X-Bacon', 0.00);
+INSERT INTO tb_sanduiche (id, nome_sanduiche, valor_sanduiche) VALUES (4, 'X-Bacon', 13.00);
+-- gere mais 10 sanduiches com nomes diferentes e valores diferentes
+INSERT INTO tb_sanduiche (id, nome_sanduiche, valor_sanduiche) VALUES (5, 'X-Frango', 15.00);
+INSERT INTO tb_sanduiche (id, nome_sanduiche, valor_sanduiche) VALUES (6, 'X-Calabresa', 15.00);
+INSERT INTO tb_sanduiche (id, nome_sanduiche, valor_sanduiche) VALUES (7, 'Hambuguer', 10.00);
+INSERT INTO tb_sanduiche (id, nome_sanduiche, valor_sanduiche) VALUES (8, 'X-Bacon Salada', 13.00);
+INSERT INTO tb_sanduiche (id, nome_sanduiche, valor_sanduiche) VALUES (9, 'X-Frango Salada', 20.00);
+INSERT INTO tb_sanduiche (id, nome_sanduiche, valor_sanduiche) VALUES (10, 'Cowboy', 18.00);
+INSERT INTO tb_sanduiche (id, nome_sanduiche, valor_sanduiche) VALUES (11, 'X-Salada Especial', 19.00);
+
+
+
+
+
+
+
+
 
 -- Inserção dos ingredientes dos sanduíches
 INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (1, 1);
@@ -59,3 +75,53 @@ INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (4, 2)
 INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (4, 4);
 INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (4, 11);
 INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (4, 13);
+
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (5, 1);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (5, 11);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (5, 6);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (5, 7);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (5, 8);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (5, 13);
+
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (6, 1);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (6, 11);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (6, 6);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (6, 7);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (6, 8);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (6, 13);
+
+-- gere para todos os sanduiches acima após o id 6 os ingredientes
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (7, 1);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (7, 2);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (7, 5);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (7, 11);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (7, 13);
+
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (8, 1);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (8, 3);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (8, 4);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (8, 11);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (8, 13);
+
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (9, 1);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (9, 2);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (9, 4);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (9, 9);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (9, 11);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (9, 13);
+
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (10, 1);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (10, 2);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (10, 4);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (10, 11);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (10, 13);
+
+-- gere os ingredientes para o sanduiche X-Salada Especial'
+
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (11, 1);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (11, 2);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (11, 4);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (11, 11);
+INSERT INTO sanduiche_ingredientes (sanduiche_id, ingredientes_id) VALUES (11, 13);
+
+
