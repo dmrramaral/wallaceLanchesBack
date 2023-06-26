779c1b88371ba0b804ea80a5794dc4dfde218cc6
Amaral <dmrramaral@gmail.com>
2023-06-26 13:51:18 -0300

    Update produto e Data

--- lanchonete/src/main/java/com/br/wallace/lanchonete/entity/Produto.java
+++ lanchonete/src/main/java/com/br/wallace/lanchonete/entity/Produto.java
@@ -1,14 +1,20 @@
 package com.br.wallace.lanchonete.entity;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 /**
- * Sanduiche
+ * Produto
  */
 @Data
 @Entity
@@ -20,12 +26,17 @@ public class Produto {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column
     private String nome;
-
+    @Column
     private String descricao;
-
+    @Column
     private Double valor;
-
+    @Column
     private Categoria categoria;
+
+    @ManyToMany
+    @JoinTable(name = "tb_produto_ingredientes", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "ingrediente_id"))
+    private List<Ingredientes> ingredientes;
 
 }
--- lanchonete/src/main/resources/data.sql
+++ lanchonete/src/main/resources/data.sql
@@ -25,37 +25,63 @@ INSERT INTO INGREDIENTES (ID, TIPO_INGREDIENTE_ID, NOME_INGREDIENTE) VALUES (9, 
 INSERT INTO INGREDIENTES (ID, TIPO_INGREDIENTE_ID, NOME_INGREDIENTE) VALUES (10, 2, 'Ervilha');
 INSERT INTO INGREDIENTES (ID, TIPO_INGREDIENTE_ID, NOME_INGREDIENTE) VALUES (11, 3, 'Queijo');
 INSERT INTO INGREDIENTES (ID, TIPO_INGREDIENTE_ID, NOME_INGREDIENTE) VALUES (12, 3, 'Presunto');
-INSERT INTO INGREDIENTES (ID, TIPO_INGREDIENTE_ID, NOME_INGREDIENTE) VALUES (13, 4, 'Pão de Hamburguer');
+INSERT INTO INGREDIENTES (ID, TIPO_INGREDIENTE_ID, NOME_INGREDIENTE) VALUES (13, 4, 'Pão');
 
 -- Inserção dos sanduíches
-INSERT INTO tb_produto (id, descricao, nome, valor, categoria) VALUES (1,' Sanduiche gostos', 'X-Tudo', 20.00, 1);
-INSERT INTO tb_produto (id, descricao, nome, valor, categoria) VALUES (2,' Sanduiche gostos', 'X-Burguer', 10.00, 1);
-INSERT INTO tb_produto (id, descricao, nome, valor, categoria) VALUES (3,' Sanduiche gostos', 'X-Salada', 15.00, 1);
-INSERT INTO tb_produto (id, descricao, nome, valor, categoria) VALUES (4,' Sanduiche gostos', 'X-Bacon', 0.00, 1);
 
--- Inserção dos ingredientes dos sanduíches
-/* INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (1, 1);
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (1, 3);
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (1, 4);
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (1, 11);
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (1, 6);
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (1, 7);
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (1, 8);
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (1, 13);
+INSERT INTO tb_produto (id, descricao, nome, valor, categoria) VALUES (1,' Sanduiche gostos', 'X-Tudo', 22.00, 1);
+INSERT INTO tb_produto (id, descricao, nome, valor, categoria) VALUES (2,' Sanduiche gostos', 'X-Burguer', 13.00, 1);
+INSERT INTO tb_produto (id, descricao, nome, valor, categoria) VALUES (3,' Sanduiche gostos', 'X-Salada Simples', 15.00, 1);
+INSERT INTO tb_produto (id, descricao, nome, valor, categoria) VALUES (4,' Sanduiche gostos', 'X-Salada Especial', 15.00, 1);
+INSERT INTO tb_produto (id, descricao, nome, valor, categoria) VALUES (5,' Sanduiche gostos', 'X-Bacon', 0.00, 1);
+INSERT INTO tb_produto (id, descricao, nome, valor, categoria) VALUES (6,' Sanduiche gostos', 'X-Bacon Salada', 0.00, 1);
+INSERT INTO tb_produto (id, descricao, nome, valor, categoria) VALUES (7,' Sanduiche gostos', 'Gostosão', 20.00, 1);
+INSERT INTO tb_produto (id, descricao, nome, valor, categoria) VALUES (8,' Sanduiche gostos', 'X-Man', 10.00, 1);
+INSERT INTO tb_produto (id, descricao, nome, valor, categoria) VALUES (9,' Sanduiche gostos', 'Gordinho Turbinado', 10.00, 1);
 
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (2, 1);
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (2, 11);
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (2, 13);
 
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (3, 1);
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (3, 11);
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (3, 6);
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (3, 7);
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (3, 8);
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (3, 13);
+-- X-Burguer
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES (2, 13); -- Pão
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES (2, 1);  -- Hambúrguer
 
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (4, 1);
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (4, 2);
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (4, 4);
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (4, 11);
-INSERT INTO produto_ingredientes (produto_id, ingredientes_id) VALUES (4, 13); */
+
+
+-- X-Salada Simples
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES (3, 13); -- Pão
+  INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES (3, 1);  -- Hambúrguer
+  INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES (3, 6);  -- Alface
+  INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES (3, 7);  -- Tomate
+
+
+-- X-Salada Especial
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES (4, 13); -- Pão
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES(4, 1); -- Hambúrguer
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES (4, 11); -- Queijo
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES(4, 3);  -- Ovo
+
+-- X-Bacon
+
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES (5, 13); -- Pão
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES (5, 1);  -- Hambúrguer
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES (5, 4);  -- Bacon
+
+-- X-Bacon Salada
+
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES (6, 13); -- Pão
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES (6, 1);  -- Hambúrguer
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES (6, 4);  -- Bacon
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES (6, 6);  -- Alface
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES (6, 7);  -- Tomate
+
+-- X-Tudo
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES(1, 13); -- Pão
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES(1, 1 ); -- Hambúrguer
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES(1, 4 ); -- Bacon
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES(1, 6 ); -- Alface
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES(1, 7 ); -- Tomate
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES(1, 8 ); -- Cebola
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES(1, 9 ); -- Milho
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES(1, 10); -- Ervilha
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES(1, 11); -- Queijo
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES(1, 12); -- Presunto
+INSERT INTO tb_produto_ingredientes (produto_id, ingrediente_id) VALUES(1, 3 ); -- Ovo
